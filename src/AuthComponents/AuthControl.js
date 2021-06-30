e4f117b9624de83867247f7d7c4e9ad034438973
PDP889 <pdp889@protonmail.com>
2021-06-29 21:02:30 -0500

    changed log in screen

--- src/AuthComponents/AuthControl.js
+++ src/AuthComponents/AuthControl.js
@@ -34,7 +34,7 @@ export default function AuthControl (props){
     } else {
         return(
             <div className='main-login-section d-flex flex-column justify-content-around'>
-                <h1 className='title d-flex justify-content-around'>Omaha Server App</h1>
+                <h2 className='title d-flex justify-content-around'>Omaha Server App</h2>
                 <div className='body-login-section d-flex justify-content-around'>
                     <button className='btn btn-primary btn-lg' onClick={toggleSignUp}>Sign Up</button>
                     <button className='btn btn-primary btn-lg' onClick={toggleLogin}>Log In</button>
